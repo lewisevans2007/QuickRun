0760b8f22e5c06626aa03ff01e53fc53b63a58f2
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-12-20 10:45:27 +0000

    Fix Bug on buttons

--- QuickRun/Info.Designer.cs
+++ QuickRun/Info.Designer.cs
@@ -40,9 +40,8 @@
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
 			this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(356, 176);
+			this.pictureBox1.Size = new System.Drawing.Size(267, 143);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
@@ -52,10 +51,9 @@
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-			this.label1.Location = new System.Drawing.Point(16, 180);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(12, 146);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(103, 25);
+			this.label1.Size = new System.Drawing.Size(83, 20);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Quick Run";
 			// 
@@ -63,10 +61,9 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.ForeColor = System.Drawing.Color.White;
-			this.label2.Location = new System.Drawing.Point(16, 209);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Location = new System.Drawing.Point(12, 170);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(147, 16);
+			this.label2.Size = new System.Drawing.Size(118, 13);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "By: Awesomelewis2007\r\n";
 			// 
@@ -74,24 +71,22 @@
 			// 
 			this.Version.AutoSize = true;
 			this.Version.ForeColor = System.Drawing.Color.White;
-			this.Version.Location = new System.Drawing.Point(16, 230);
-			this.Version.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Version.Location = new System.Drawing.Point(12, 187);
 			this.Version.Name = "Version";
-			this.Version.Size = new System.Drawing.Size(96, 16);
+			this.Version.Size = new System.Drawing.Size(81, 13);
 			this.Version.TabIndex = 3;
 			this.Version.Text = "Version: 0.0.0.0";
 			// 
 			// Info
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
-			this.ClientSize = new System.Drawing.Size(353, 260);
+			this.ClientSize = new System.Drawing.Size(265, 211);
 			this.Controls.Add(this.Version);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "Info";
 			this.Text = "About QuickRun";
 			this.Load += new System.EventHandler(this.Info_Load);
